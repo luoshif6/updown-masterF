09afbf56d402088634d72bdadefce8121bf5a666
yanhao <877230054@qq.com>
2019-08-26 18:27:03 +0800

    增加登录清空缓存

--- updown-sso-web/src/main/java/com/updown/sso/controller/UserLoginController.java
+++ updown-sso-web/src/main/java/com/updown/sso/controller/UserLoginController.java
@@ -4,6 +4,7 @@ import com.updown.common.pojo.UpdownResult;
 import com.updown.common.utils.CookieUtils;
 import com.updown.common.utils.JsonUtils;
 import com.updown.pojo.User;
+import com.updown.service.FileService;
 import com.updown.sso.service.UserLoginService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -22,23 +23,29 @@ public class UserLoginController {
     @Autowired
     private UserLoginService userLoginService;
 
+    @Autowired
+    private FileService fileService;
+
     @Value("${UP_TOKEN_KEY}")
     private String UP_TOKEN_KEY;
 
     /**
      * 用户登录,查询用户
+     *
      * @return
      */
     @CrossOrigin
-    @RequestMapping(value = "login",method = RequestMethod.POST)
+    @RequestMapping(value = "login", method = RequestMethod.POST)
     public ResponseEntity<UpdownResult> findUser(@RequestBody String params,
                                                  HttpServletRequest request,
-                                                 HttpServletResponse response){
+                                                 HttpServletResponse response) {
+//        清空pdf缓存
+        fileService.deletePDFCache();
         User user = JsonUtils.jsonToPojo(params, User.class);
-        UpdownResult updownResult = this.userLoginService.findUser(user.getUser_name(),user.getUser_password());
-        if (updownResult.getStatus() == 200){  //用户名密码都正确,登录成功
+        UpdownResult updownResult = this.userLoginService.findUser(user.getUser_name(), user.getUser_password());
+        if (updownResult.getStatus() == 200) {  //用户名密码都正确,登录成功
             //设置token到cookie中
-            CookieUtils.setCookie(request,response,UP_TOKEN_KEY,updownResult.getData().toString());
+            CookieUtils.setCookie(request, response, UP_TOKEN_KEY, updownResult.getData().toString());
             //登录成功后根据token获取用户信息返回给页面
             UpdownResult result = this.userLoginService.findUserByToken(updownResult.getData().toString());
             return ResponseEntity.ok(result);
@@ -49,13 +56,14 @@ public class UserLoginController {
 
     /**
      * 根据token获取用户信息
+     *
      * @param token
      * @return
      */
-    @RequestMapping(value = "token/{token}",method = RequestMethod.GET)
-    public ResponseEntity<UpdownResult> getUserByToken(@PathVariable("token") String token){
+    @RequestMapping(value = "token/{token}", method = RequestMethod.GET)
+    public ResponseEntity<UpdownResult> getUserByToken(@PathVariable("token") String token) {
         UpdownResult result = this.userLoginService.findUserByToken(token);
-        if (result.getStatus() == 200){
+        if (result.getStatus() == 200) {
             //将用户密码设置为空
             User user = (User) result.getData();
             user.setUser_password(null);

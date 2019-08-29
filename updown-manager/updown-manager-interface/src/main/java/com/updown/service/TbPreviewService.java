1e24f4e446aaea584fdcde0ca46ebc61e9fde864
hewenze <184557955@qq.com>
2019-08-29 17:17:16 +0800

    预览表

---
+++ updown-manager/updown-manager-interface/src/main/java/com/updown/service/TbPreviewService.java
@@ -0,0 +1,12 @@
+package com.updown.service;
+import com.updown.common.pojo.UpdownResult;
+import com.updown.pojo.Preview;
+public interface TbPreviewService {
+
+    //添加记录
+    UpdownResult insertTbPreview(Preview preview);
+    //删除记录
+    UpdownResult deleteTbPreviewByUserId(Long user_id);
+    //根据file_id查预览文件
+    Preview selectByFileId(Long file_id);
+}
